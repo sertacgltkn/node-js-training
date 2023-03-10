1dadfe58b2be349850e11e56cacea542632b01a1
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-10 20:51:19 +0300

    quiz app

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "MainPage") {
-    res.end("HomePage");
-  } 
-  else if(pathName === "/questions"){
-    res.writeHead()
-  }
-});
-server.listen(3000, () => {
-  console.log("Server is running on 3000 port");
-});
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const { dirname } = require('path')
+
+const htmldosyam = fs.readFileSync(`${__dirname}/index.html`,'utf-8')
+
+const server=http.createServer((req,res)=>{
+    const pathName=req.url
+    if(pathName==='/'){
+        res.end('HomePage')
+    }
+    else if(pathName==='/questions'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(htmldosyam)
+    }
+})
+
+server.listen(3000,()=>{
+    console.log('Server is running on 3000 port')
+})
