6c58bda594e1175ccd11f91ff5b93c26248fc654
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-11 13:10:30 +0300

    fs appendFile

--- app.js
+++ app.js
@@ -33,9 +33,8 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+    fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
   });
 }).listen(3000);
