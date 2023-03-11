69f0f1462068f92d1c7cdb2c286c7f3909b7559e
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-11 13:06:19 +0300

    file system read

--- app.js
+++ app.js
@@ -20,10 +20,22 @@
 //     console.log('Server is running on 3000 port')
 // })
 
+// var http = require('http');
+// var dt = require('./myfirstmodule');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write("The date and time are currently: " + dt.myDateTime());
+//   res.end();
+// }).listen(8080);
+
 
 var http = require('http');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(3000);
