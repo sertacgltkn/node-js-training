d66058f33b3ba510fb9a03dc3b064035fb152ad3
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-11 11:49:28 +0300

    w3

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const { dirname } = require('path')
+// const http = require('http')
+// const url = require('url')
+// const fs = require('fs')
+// const { dirname } = require('path')
 
-const htmldosyam = fs.readFileSync(`${__dirname}/index.html`,'utf-8')
+// const htmldosyam = fs.readFileSync(`${__dirname}/index.html`,'utf-8')
 
-const server=http.createServer((req,res)=>{
-    const pathName=req.url
-    if(pathName==='/'){
-        res.end('HomePage')
-    }
-    else if(pathName==='/questions'){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.end(htmldosyam)
-    }
-})
+// const server=http.createServer((req,res)=>{
+//     const pathName=req.url
+//     if(pathName==='/'){
+//         res.end('HomePage')
+//     }
+//     else if(pathName==='/questions'){
+//         res.writeHead(200,{'Content-Type':'text/html'});
+//         res.end(htmldosyam)
+//     }
+// })
 
-server.listen(3000,()=>{
-    console.log('Server is running on 3000 port')
-})
+// server.listen(3000,()=>{
+//     console.log('Server is running on 3000 port')
+// })
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(3000);
