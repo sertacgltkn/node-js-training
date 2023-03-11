a038e4ffea01f76fef6fd7a844b966b724f0c45c
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-11 14:39:43 +0300

    upload files

--- app.js
+++ app.js
@@ -29,7 +29,6 @@
 //   res.end();
 // }).listen(8080);
 
-
 // var http = require('http');
 // var fs = require('fs');
 
@@ -39,7 +38,6 @@
 //         console.log('File Renamed!');
 //       });
 // }).listen(3000);
-
 
 // var url = require('url');
 // var adr = 'http://localhost:3000/default.htm?year=2017&month=february';
@@ -52,21 +50,69 @@
 // var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
 // console.log(qdata.month); //returns 'february'
 
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
+
+// http.createServer(function (req, res) {
+//   var q = url.parse(req.url, true);
+//   var filename = "." + q.pathname;
+//   fs.readFile(filename, function(err, data) {
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'});
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
+// }).listen(3000);
+
+// var http = require('http');
+// var uc = require('upper-case');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write(uc.upperCase("Hello World!"));
+//   res.write(uc.upperCase("merhabalar"))
+//   res.end();
+// }).listen(8080);
+
+// var events = require('events');
+// var eventEmitter = new events.EventEmitter();
+
+// //Create an event handler:
+// var myEventHandler = function () {
+//   console.log('I hear a scream!');
+// }
+
+// //Assign the event handler to an event:
+// eventEmitter.on('scream', myEventHandler);
+
+// //Fire the 'scream' event:
+// eventEmitter.emit('scream');
 
 var http = require('http');
-var url = require('url');
+var formidable = require('formidable');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
     return res.end();
-  });
+  }
 }).listen(3000);
