b4f45fc260caec4592c1aaeea298338dfdc6aa14
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-11 13:32:40 +0300

    url.parse()

--- app.js
+++ app.js
@@ -30,11 +30,24 @@
 // }).listen(8080);
 
 
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-        if (err) throw err;
-        console.log('File Renamed!');
-      });
-}).listen(3000);
+// var http = require('http');
+// var fs = require('fs');
+
+// http.createServer(function (req, res) {
+//     fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+//         if (err) throw err;
+//         console.log('File Renamed!');
+//       });
+// }).listen(3000);
+
+
+var url = require('url');
+var adr = 'http://localhost:3000/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
