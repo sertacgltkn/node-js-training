aefb28e74ee26dedbff67515fe4b59d7dfd9692b
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-03-13 17:59:37 +0300

    JSON & get method

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync(`${__dirname}/countries.json`));
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/countries", (req, res) => {
+    res.send(data);
+});
 
 
-app.get('/',((req,res)=>{
-    res.send('Home');
-}))
+app.post("/post", (req, res) => {
+  res.send("Post sent");
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
